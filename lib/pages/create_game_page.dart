26f252bb2674d4a94c76b3fe5ec67e20fc81a568
Niki <nl@auxcode.com>
2022-04-28 01:25:13 +0300

    set amount of rounds, make the widget stateful

--- lib/pages/create_game_page.dart
+++ lib/pages/create_game_page.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
-class CreateGamePage extends StatelessWidget {
+class CreateGamePage extends StatefulWidget {
   const CreateGamePage({Key? key}) : super(key: key);
 
+  @override
+  State<StatefulWidget> createState() => _CreatePageState();
+}
+class _CreatePageState extends State<CreateGamePage> {
+
+  int dropdownValue = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,21 +20,50 @@ class CreateGamePage extends StatelessWidget {
           heightFactor: 0.9,
           child: DecoratedBox(
             decoration: BoxDecoration(
-              border: Border.all(color: Colors.black, width: 4.0)
-            ),
+                border: Border.all(color: Colors.black, width: 4.0)),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: const <Widget>[
-                Text('Code', style: TextStyle(fontSize: 20),),
-                TextField(
+              children: <Widget>[
+                const Text(
+                  'Код за достъп',
+                  style: TextStyle(fontSize: 20),
+                ),
+                const TextField(
                   enabled: false,
                   decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Random genereted code for room"
-                  ),
+                      border: OutlineInputBorder(),
+                      labelText: "Random genereted code for room"),
                 ),
-              ].map( (el) => Padding(padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 25), child: el,)).toList(),
+                const Text(
+                  'Брой рундове',
+                  style: TextStyle(fontSize: 20),
+                ),
+                DropdownButton(
+                  value: dropdownValue,
+                  style: const TextStyle(color: Colors.black),
+                  items: <int>[1, 2]
+                      .map<DropdownMenuItem<int>>((int? value) {
+                    return DropdownMenuItem<int>(
+                      value: value,
+                      child: Text(value.toString()),
+                    );
+                  }).toList(),
+                  onChanged: (int? newValue) {
+                    // set state 
+                    setState(() {
+                      dropdownValue = newValue!;
+                    });
+                  },
+                  
+                ),
+              ]
+                  .map((el) => Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 10, horizontal: 25),
+                        child: el,
+                      ))
+                  .toList(),
             ),
           ),
         ),
