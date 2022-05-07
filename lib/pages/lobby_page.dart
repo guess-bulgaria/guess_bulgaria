c1024b94de3a415e274ed525d100a1f507b06bf5
Niki <nl@auxcode.com>
2022-05-07 22:17:42 +0300

    enable putting 0 on tthe timer

--- lib/pages/lobby_page.dart
+++ lib/pages/lobby_page.dart
@@ -382,7 +382,8 @@ class _LobbyPageState extends State<LobbyPage> {
                   int time = int.tryParse(value!) ?? 0;
                   if (time > 120) {
                     _timeController.text = "120";
-                  }else if(time < 5){
+                  }
+                  if (time > 0 && time < 5) {
                     _timeController.text = "5";
                   }
                   return null;
